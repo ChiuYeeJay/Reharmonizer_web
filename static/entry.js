0bee5367eaf6a61f8461647fb73f99d2c1fe5b4f
ChiuYeeJay <shes202@gmail.com>
2022-09-05 22:15:03 +0800

    using post_sender to send request

--- static/entry.js
+++ static/entry.js
@@ -2,60 +2,50 @@
 
 var audio_id = ""
 
-function harmonization_suceed(){
-    if (this.readyState != XMLHttpRequest.DONE) return;
-    if (this.status === 200) {
-        document.getElementById("upload_status").innerText = "redirect..."
-        window.location.assign(this.responseText)
+function post_sender(action, data, handler, content_type="", response_type=""){
+    let xhttp_request = new XMLHttpRequest();
+    xhttp_request.onreadystatechange = ()=>{
+        if (xhttp_request.readyState != XMLHttpRequest.DONE) return;
+        if (xhttp_request.status !== 200) {
+            console.warn("something wrong in " + action + ": " + xhttp_request.status);
+            window.alert("something wrong: " + xhttp_request.status)
+            return;
+        }
+
+        handler(xhttp_request)
     }
-    else{
-        console.warn("something wrong: " + this.status);
-        window.alert("something wrong: " + this.status)
-    }
+    xhttp_request.open("POST", action);
+    if(response_type != "") xhttp_request.responseType = response_type
+    if(content_type != "") xhttp_request.setRequestHeader("Content-Type", content_type+";charset=UTF-8");
+    xhttp_request.send(data);
+}
+
+function harmonization_suceed(xhttp_request){
+    document.getElementById("upload_status").innerText = "redirect..."
+    window.location.assign(xhttp_request.responseText)
 }
 
 function start_harmonization(){
-    let harmonization_args = {"octave":4}
-    let harmonization_request = new XMLHttpRequest();
-    harmonization_request.onreadystatechange = harmonization_suceed
-    harmonization_request.open("POST", "/harmonize");
-    harmonization_request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    harmonization_request.send(JSON.stringify({"args":harmonization_args, "audio_id":audio_id}));
+    let harmonization_data = JSON.stringify({"args":{"octave":4}, "audio_id":audio_id});
+    post_sender("/harmonize", harmonization_data, harmonization_suceed, 
+        "application/json", "text");
 }
 
-function audio2midi_suceeded(){
-    if (this.readyState != XMLHttpRequest.DONE) return;
-    if (this.status === 200) {
-        document.getElementById("upload_status").innerText = "harmonizing..."
-        start_harmonization()
-    }
-    else{
-        console.warn("something wrong: " + this.status);
-        window.alert("something wrong: " + this.status)
-    }
+function audio2midi_suceeded(xhttp_request){
+    document.getElementById("upload_status").innerText = "harmonizing..."
+    start_harmonization()
 }
 
 function start_audio2midi(){
-    let audio2midi_request = new XMLHttpRequest();
-    audio2midi_request.onreadystatechange = audio2midi_suceeded
-    audio2midi_request.open("POST", "/audio2midi");
-    audio2midi_request.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
-    audio2midi_request.send(JSON.stringify({"audio_id":audio_id}));
+    post_sender("/audio2midi", JSON.stringify({"audio_id":audio_id}), audio2midi_suceeded,
+                "application/json")
 }
 
-function submit_suceeded(){
-    if (this.readyState != XMLHttpRequest.DONE) return;
-    if (this.status === 200) {
-        // console.log(this.responseText);
-        audio_id = this.response.audio_id;
-        console.log("audio_id: " + audio_id)
-        document.getElementById("upload_status").innerText = "processing audio..."
-        start_audio2midi()
-    }
-    else{
-        console.warn("something wrong: " + this.status);
-        window.alert("something wrong: " + this.status)
-    }
+function submit_suceeded(xhttp_request){
+    audio_id = xhttp_request.response.audio_id;
+    console.log("audio_id: " + audio_id)
+    document.getElementById("upload_status").innerText = "processing audio..."
+    start_audio2midi()
 }
 
 function submit_audio(){
@@ -63,11 +53,7 @@ function submit_audio(){
     form_element.hidden = true;
     document.getElementById("upload_status").innerText = "uploading audio..."
     
-    let audio_submit_request = new XMLHttpRequest();
-    audio_submit_request.onreadystatechange = submit_suceeded
-    audio_submit_request.open("POST", form_element.action);
-    audio_submit_request.responseType = "json"
-    audio_submit_request.send(new FormData(form_element));
+    post_sender(form_element.action, new FormData(form_element), submit_suceeded, "", "json");
 }
 
 function on_audio_chosen(){
