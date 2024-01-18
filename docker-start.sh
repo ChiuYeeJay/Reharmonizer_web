aad9a6d9dcfb66f2f8358483e6f3625794a8624f
ChiuYeeJay <shes202@gmail.com>
2024-01-18 23:22:54 +0800

    Add auto_tempfile_cleaner.py to docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,4 @@
 rabbitmq-server >& ./rabbitmq.log &
 celery -A celery_tasks worker --loglevel=INFO >& ./celery.log &
+python3.9 auto_tempfile_cleaner.py &
 gunicorn -b 0.0.0.0:8000 --timeout 120 app:app
